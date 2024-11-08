18d5665e3ddcd4a47d9af8a51eaa72e516cc6038
Coppermine-SP <son5121502@gmail.com>
2024-11-09 01:46:50 +0900

    migration: change category expiredate to nullable

---
+++ src/Migrations/20241108153653_SecondMigration.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace fridgeplus_server.Migrations
+{
+    /// <inheritdoc />
+    public partial class SecondMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ItemQuantity",
+                table: "Items",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AlterColumn<TimeSpan>(
+                name: "Expires",
+                table: "Categories",
+                type: "time(6)",
+                nullable: true,
+                oldClrType: typeof(TimeSpan),
+                oldType: "time(6)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ItemQuantity",
+                table: "Items");
+
+            migrationBuilder.AlterColumn<TimeSpan>(
+                name: "Expires",
+                table: "Categories",
+                type: "time(6)",
+                nullable: false,
+                defaultValue: new TimeSpan(0, 0, 0, 0, 0),
+                oldClrType: typeof(TimeSpan),
+                oldType: "time(6)",
+                oldNullable: true);
+        }
+    }
+}
