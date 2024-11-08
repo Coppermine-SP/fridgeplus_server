10c3ec743d3e5126789477cc9c7980c69918be31
Coppermine-SP <son5121502@gmail.com>
2024-11-09 01:50:30 +0900

    add: FridgeController API definitions

--- src/Controllers/FridgeController.cs
+++ src/Controllers/FridgeController.cs
@@ -1,5 +1,11 @@
-﻿using fridgeplus_server.Context;
-using Microsoft.AspNetCore.Http;
+﻿/*
+    FridgeController - fridgeplus_server
+    Copyright (C) 2024-2025 Coppermine-SP
+ */
+
+
+using fridgeplus_server.Context;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace fridgeplus_server.Controllers
@@ -16,5 +22,35 @@ namespace fridgeplus_server.Controllers
             _logger = logger;
             _dbContext = context;
         }
+
+        [HttpGet]
+        [Authorize]
+        public IActionResult CategoryList()
+        {
+            return Ok();
+        }
+
+        [HttpGet]
+        [Authorize]
+        public IActionResult ItemList()
+        {
+            return Ok();
+        }
+
+        [HttpPost]
+        [Authorize]
+        public IActionResult AddItems()
+        {
+            return Ok();
+        }
+
+        [HttpGet]
+        [Authorize]
+        public IActionResult DeleteItem(string id)
+        {
+            return Ok();
+        }
+
+
     }
 }
