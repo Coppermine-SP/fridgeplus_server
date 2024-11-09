2adada0a52004bbca8db025addaee197430b3913
Coppermine-SP <son5121502@gmail.com>
2024-11-10 02:30:38 +0900

    chore: connectionString null check

--- src/Context/ServerDbContext.cs
+++ src/Context/ServerDbContext.cs
@@ -9,6 +9,11 @@ namespace fridgeplus_server.Context
         public DbSet<Category> Categories { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder options)
-            => options.UseMySQL(Environment.GetEnvironmentVariable("MYSQL_CONNECTION_STRING"));
+        {
+            string? connectionString = Environment.GetEnvironmentVariable("MYSQL_CONNECTION_STRING");
+
+            if (connectionString is null) throw new ArgumentException("Environment variable MYSQL_CONNECTION_STRING was null.");
+            options.UseMySQL(connectionString);
+        }
     }
 }
