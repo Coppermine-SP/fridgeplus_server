9b9c85a2d6ab7b93f05244dc461d4e66154a8a93
copperminesp <son5121502@gmail.com>
2024-11-24 21:11:30 +0900

    feat: json schema cache

--- src/Services/OpenAIGPTService.cs
+++ src/Services/OpenAIGPTService.cs
@@ -1,6 +1,7 @@
 ﻿using System.Text.Encodings.Web;
 using System.Text.Json;
 using System.Text.Unicode;
+using Mysqlx.Datatypes;
 using OpenAI;
 using OpenAI.Chat;
 using Newtonsoft.Json.Schema;
@@ -11,13 +12,28 @@ namespace fridgeplus_server.Services
     public class OpenAIGPTService : IChatCompletionService
     {
         private readonly ILogger _logger;
-        private readonly OpenAIClient _client;
         private readonly ChatClient _chatClient;
+        private readonly Dictionary<Type, BinaryData> _schemaDictionary;
         private readonly JsonSerializerOptions _serializerOptions = new JsonSerializerOptions()
         {
             Encoder = JavaScriptEncoder.Create(UnicodeRanges.All)
         };
 
+        private BinaryData _getSchemaFromCache(Type type)
+        {
+            if (_schemaDictionary.TryGetValue(type, out BinaryData? value)) return value;
+            
+            _logger.Log(LogLevel.Information, $"Type {type} is not in json schema cache! Generating json schema...");
+                
+            JSchemaGenerator generator = new JSchemaGenerator();
+            JSchema schema = generator.Generate(type);
+            schema.AdditionalProperties = JSchema.Parse("false");
+
+            BinaryData binary = BinaryData.FromString(schema.ToString());
+            _schemaDictionary.Add(type, binary);
+            return binary;
+        }
+        
         public OpenAIGPTService(ILogger<OpenAIGPTService> logger)
         {
             _logger = logger;
@@ -32,8 +48,9 @@ namespace fridgeplus_server.Services
                 throw new ArgumentException();
             }
 
-            _client = new OpenAIClient(key);
-            _chatClient = _client.GetChatClient(model);
+            var client = new OpenAIClient(key);
+            _chatClient = client.GetChatClient(model);
+            _schemaDictionary = new Dictionary<Type, BinaryData>();
         }
 
         public JsonDocument? ChatCompletion(string taskId, string prompt, object data, Type responseFormat)
@@ -47,17 +64,13 @@ namespace fridgeplus_server.Services
                     new SystemChatMessage(prompt),
                     new UserChatMessage(JsonSerializer.Serialize(data, _serializerOptions))
                 };
-
-                JSchemaGenerator generator = new JSchemaGenerator();
-                JSchema schema = generator.Generate(responseFormat);
-                schema.AdditionalProperties = JSchema.Parse("false");
-
+                
                 ChatCompletionOptions options = new()
                 {
                     ResponseFormat = ChatResponseFormat.CreateJsonSchemaFormat(
                         jsonSchemaFormatName: responseFormat.Name,
                         jsonSchemaIsStrict: false,
-                        jsonSchema: BinaryData.FromString(schema.ToString()))
+                        jsonSchema: _getSchemaFromCache(responseFormat))
                 };
 
                 _logger.LogInformation($"#{taskId}: Waiting response from OpenAI..");
